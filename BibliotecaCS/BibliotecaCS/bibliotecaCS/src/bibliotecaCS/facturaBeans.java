d1d18b14bb4acaf0a6c16806d6cd37afcf5c9094
NerexiAnahi <nerexianahi2003@gmail.com>
2023-09-04 18:51:39 -0500

    Modificacion de factura y libro

--- BibliotecaCS/BibliotecaCS/bibliotecaCS/src/bibliotecaCS/facturaBeans.java
+++ BibliotecaCS/BibliotecaCS/bibliotecaCS/src/bibliotecaCS/facturaBeans.java
@@ -1,4 +1,3 @@
-
 package miprimeraaplicacioncs;
 
 import java.sql.ResultSet;
@@ -14,95 +13,143 @@ public class facturaBeans {
         return idFactura;
     }
 
-    public void setIdFactura(int idFactura) {
+    public void setIdFactura(int idFactura) {       /* 1*/
         this.idFactura = idFactura;
     }
-
+     /* 
+        1/ Ta
+        T= Ta
+    */
     public int getIdUsuario() {
         return idUsuario;
     }
 
-    public void setIdUsuario(int idUsuario) {
+    public void setIdUsuario(int idUsuario) {        /* 1*/
         this.idUsuario = idUsuario;
     }
-
+    /* 
+        1/ Ta
+        T= Ta
+    */
     public int getIVA() {
         return IVA;
     }
 
-    public void setIVA(int IVA) {
+    public void setIVA(int IVA) {                /* 1*/
         this.IVA = IVA;
     }
-
-    public int getTotal() {
+    /* 
+        1/ Ta
+        T= Ta
+    */
+    public int getTotal() {                 
         return total;
     }
 
-    public void setTotal(int total) {
+    public void setTotal(int total) {          /* 1*/
         this.total = total;
     }
-
+     /* 
+        1/ Ta
+        T= Ta
+    */
     public accesobd getBd() {
         return bd;
     }
 
-    public void setBd(accesobd bd) {
+    public void setBd(accesobd bd) {             /* 1*/
         this.bd = bd;
     }
 
-    
+    /* 
+        1/ Ta
+        T= Ta
+    */
     accesobd bd;
 
     public facturaBeans() throws Exception {
-        bd = new accesobd("localhost", "root", "", "bliblioteca");
-        bd.conectarBD();
+        bd = new accesobd("localhost", "root", "", "bliblioteca");   /* 1*/
+        bd.conectarBD();                                             /* 2 */
     }
-
+/* 
+1/ Ta
+2/ 3ta
+T= 5Ta+3TAa=8Ta
+*/
     
-    public int Incremento_factura() throws SQLException {
+    public int Incremento_factura() throws SQLException {        
         int inc;
         ResultSet rs;
-        rs = bd.consultaBD("SELECT max(idFactura) as num FROM factura;");
-        if (rs.next()) {
-            inc = rs.getInt(1) + 1;
-        } else {
-            inc = 1;
+        rs = bd.consultaBD("SELECT max(idFactura) as num FROM factura;"); /* 3*/
+        if (rs.next()) {                                                  /* 4*/
+            inc = rs.getInt(1) + 1;                                       /* 5*/
+        } else {                                                          /* 6*/
+            inc = 1;                                                      /* 7*/
         }
         return inc;
     }
-
+    /* 
+        3/ Ta
+        4/ Tc
+        5/ Ta+To
+        7/ Ta
+       
+        TP = Ta+Tc+Ta+To = 2Ta+Tc+To
+        TM = Ta+Tc+Ta = 2Ta+Tc
+        TE = 2Ta+Tc+To - 2Ta+Tc = To
+    */
     public void Insertar_factura() throws SQLException {
         String cadena = "insert into factura values('" + Incremento_factura() + "','" + getIdUsuario() + "','" + getIVA() + "','" + getTotal() + "')";
-        bd.ActualizarBD(cadena);
+        bd.ActualizarBD(cadena);                                                                                                                        /*1*/  
 
     }
-
+    /* 
+        1/ Ta
+        2/ 3Ta
+        T= Ta+3Ta =4Ta
+    */
     public void Actualizar_factura() throws SQLException {
         String cadena = "update usuario set idUsuario='" + getIdUsuario()+ "', iva='" + getIVA()+ "', total='" + getTotal()+ "'where idFactura='" +getIdFactura()+"'";
         bd.ActualizarBD(cadena);
         
                 
     }
+    /* 
+        1/ Ta
+        2/ 3Ta
+        T= Ta+3Ta =4Ta
+    */
 
     public void Eliminar_factura() throws SQLException {
-        String cadena = "delete from factura where idFactura='" + getIdFactura()+"'";
-        bd.ActualizarBD(cadena);
+        String cadena = "delete from factura where idFactura='" + getIdFactura()+"'";  /*1*/
+        bd.ActualizarBD(cadena);                                                       /*2*/
     }
-
+    /* 
+        1/ Ta
+        2/ 2Ta
+        T= Ta+2Ta =3Ta
+    */
     public ResultSet consultaTabla(String sql) throws SQLException {
         return bd.consultaBD(sql);
     }
     
     public void Consultar_factura() throws SQLException {
         ResultSet rs;
-        rs = bd.consultaBD("select * from factura");
-        while (rs.next()) {
+        rs = bd.consultaBD("select * from factura");         /*1*/ 
+        while (rs.next()) {                                  /*2*/ 
             System.out.print(rs.getInt(1) + " ");
             System.out.print(rs.getInt(2) + " ");
             System.out.print(rs.getInt(3) + " ");
             System.out.print(rs.getInt(4) + " ");
             System.out.println("");
         }
+        /* 
+        1/ 2Ta
+        2/ n*Tc+Tc
+        TP= 2Ta+n*Tc+Tc
+        TM= 2Ta+Tc
+        TE= 2Ta+n*Tc+Tc - 2Ta+Tc = n*Tc
+    */   
     }
 
 }
--- BibliotecaCS/BibliotecaCS/bibliotecaCS/src/bibliotecaCS/libroBeans.java
+++ BibliotecaCS/BibliotecaCS/bibliotecaCS/src/bibliotecaCS/libroBeans.java
@@ -12,26 +12,41 @@ public class libroBeans {
     private int idAutor;
 
     
-    public void setIdLibro(int idLibro) {
+    public void setIdLibro(int idLibro) {                   /* 1*/
         this.idLibro = idLibro;
     }
-
-    public void setTitulo(String titulo) {
+     /* 
+        1/ Ta
+        T= Ta
+    */
+    public void setTitulo(String titulo) {                 /* 1*/
         this.titulo = titulo;
     }
-
-    public void setISBN(int ISBN) {
+    /* 
+        1/ Ta
+        T= Ta
+    */
+    public void setISBN(int ISBN) {                       /* 1*/
         this.ISBN = ISBN;
     }
-
-    public void setEditorial(String editorial) {
+    /* 
+        1/ Ta
+        T= Ta
+    */
+    public void setEditorial(String editorial) {         /* 1*/
         this.editorial = editorial;
     }
-
-    public void setPaginas(int paginas) {
+/* 
+        1/ Ta
+        T= Ta
+    */ 
+    public void setPaginas(int paginas) {              /* 1*/
         this.paginas = paginas;
     }
-
+/* 
+        1/ Ta
+        T= Ta
+    */
     public void setIdAutor(int idAutor) {
         this.idAutor = idAutor;
     }
@@ -64,41 +79,68 @@ public class libroBeans {
     accesobd bd;
 
     public libroBeans() throws Exception {
-        bd = new accesobd("localhost", "root", "", "bliblioteca");
-        bd.conectarBD();
+        bd = new accesobd("localhost", "root", "", "bliblioteca"); /*1*/
+        bd.conectarBD();                                           /*2*/
     }
+    /* 
+    1/ Ta
+    2/ 3ta
+    T= 5Ta+3TAa=8Ta
+    */
 
     
     public int Incremento_libro() throws SQLException {
         int inc;
         ResultSet rs;
-        rs = bd.consultaBD("SELECT max(idLibro) as num FROM libro;");
-        if (rs.next()) {
-            inc = rs.getInt(1) + 1;
-        } else {
+        rs = bd.consultaBD("SELECT max(idLibro) as num FROM libro;"); /*3*/
+        if (rs.next()) {                                              /*4*/
+            inc = rs.getInt(1) + 1;                                   /*5*/
+        } else {                                                      /*6*/
             inc = 1;
         }
         return inc;
     }
+    /* 
+        3/ Ta
+        4/ Tc
+        5/ Ta+To
+        7/ Ta
+       
+        TP = Ta+Tc+Ta+To = 2Ta+Tc+To
+        TM = Ta+Tc+Ta = 2Ta+Tc
+        TE = 2Ta+Tc+To - 2Ta+Tc = To
+    */
 
     public void Insertar_libro() throws SQLException {
         String cadena = "insert into libro values('" + Incremento_libro() + "','" + getTitulo() + "','" + getISBN() + "','" + getEditorial()+ "','" + getPaginas() + "','" + getIdAutor()+ "')";
         bd.ActualizarBD(cadena);
 
     }
-
+    /* 
+        1/ Ta
+        2/ 3Ta
+        T= Ta+3Ta =4Ta
+    */
     public void Actualizar_libro() throws SQLException {
         String cadena = "update libro set titulo='" + getTitulo()+ "', ISBN='" + getISBN()+ "', editorial='" + getEditorial()+ "', paginas='" + getPaginas()+ "'where idLibro ='" +getIdLibro()+"'";
         bd.ActualizarBD(cadena);
         
                 
     }
-
+    /* 
+        1/ Ta
+        2/ 3Ta
+        T= Ta+3Ta =4Ta
+    */
     public void Eliminar_libro() throws SQLException {
-        String cadena = "delete from libro where idLibro='" + getIdLibro()+"'";
-        bd.ActualizarBD(cadena);
+        String cadena = "delete from libro where idLibro='" + getIdLibro()+"'";        /*1*/   
+        bd.ActualizarBD(cadena);                                                       /*2*/ 
     }
-
+    /* 
+        1/ Ta
+        2/ 2Ta
+        T= Ta+2Ta =3Ta
+    */
     public ResultSet consultaTabla(String sql) throws SQLException {
         return bd.consultaBD(sql);
     }
@@ -115,8 +157,9 @@ public class libroBeans {
 
     public void Consultar_libro() throws SQLException {
         ResultSet rs;
-        rs = bd.consultaBD("select * from libro");
-        while (rs.next()) {
+        rs = bd.consultaBD("select * from libro");           /*1*/ 
+        while (rs.next()) {                                  /*2*/                                
+        while (rs.next()) {                                  
             System.out.print(rs.getInt(1) + " ");
             System.out.print(rs.getString(2) + " ");
             System.out.print(rs.getInt(3) + " ");
@@ -125,7 +168,13 @@ public class libroBeans {
             System.out.print(rs.getInt(6) + " ");
             System.out.println("");
         }
+        /* 
+        1/ 2Ta
+        2/ n*Tc+Tc
+        TP= 2Ta+n*Tc+Tc
+        TM= 2Ta+Tc
+        TE= 2Ta+n*Tc+Tc - 2Ta+Tc = n*Tc
+    */ 
     }
 
 }
-
