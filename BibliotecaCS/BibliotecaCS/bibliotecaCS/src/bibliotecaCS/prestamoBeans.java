c366fffbba4d0e8359dd0c1db00090b5b47b4477
jonathanD28 <dlcsjonathan799@gmail.com>
2023-09-04 18:28:37 -0500

    modificaion de prestamo y usuario

--- BibliotecaCS/BibliotecaCS/bibliotecaCS/src/bibliotecaCS/prestamoBeans.java
+++ BibliotecaCS/BibliotecaCS/bibliotecaCS/src/bibliotecaCS/prestamoBeans.java
@@ -18,97 +18,150 @@ public class prestamoBeans {
     }
 
     public void setPrecio(Double precio) {
-        this.precio = precio;
+        this.precio = precio;/*1*/
     }
-
-
+    /* 
+        1/ TA
+        T= TA
+    */
     public int getIdPrestamo() {
         return idPrestamo;
     }
 
     public void setIdPrestamo(int idPrestamo) {
-        this.idPrestamo = idPrestamo;
+        this.idPrestamo = idPrestamo;/*1*/
     }
+    /* 
+        1/ TA
+        T= TA
+    */
 
     public int getIdUsuario() {
         return idUsuario;
     }
 
     public void setIdUsuario(int idUsuario) {
-        this.idUsuario = idUsuario;
+        this.idUsuario = idUsuario;/*1*/
     }
+    /* 
+        1/ TA
+        T= TA
+    */
 
     public int getIdEjemplar() {
         return idEjemplar;
     }
 
     public void setIdEjemplar(int idEjemplar) {
-        this.idEjemplar = idEjemplar;
+        this.idEjemplar = idEjemplar;/*1*/
     }
+    /* 
+        1/ TA
+        T= TA
+    */
 
     public String getFechaprest() {
         return fechaprest;
     }
 
     public void setFechaprest(String fechaprest) {
-        this.fechaprest = fechaprest;
+        this.fechaprest = fechaprest;/*1*/
     }
+    /* 
+        1/ TA
+        T= TA
+    */
 
     public String getFechadevo() {
         return fechadevo;
     }
 
     public void setFechadevo(String fechadevo) {
-        this.fechadevo = fechadevo;
+        this.fechadevo = fechadevo;/*1*/
     }
+    /* 
+        1/ TA
+        T= TA
+    */
 
     public accesobd getBd() {
         return bd;
     }
 
     public void setBd(accesobd bd) {
-        this.bd = bd;
+        this.bd = bd;/*1*/
     }
-
-   
-  
+    /* 
+        1/ TA
+        T= TA
+    */
     accesobd bd;
 
     public prestamoBeans() throws Exception {
-        bd = new accesobd("localhost", "root", "", "bliblioteca");
-        bd.conectarBD();
+        bd = new accesobd("localhost", "root", "", "bliblioteca"); /*1*/
+        bd.conectarBD();                                           /*2*/
     }
-
+    /* 
+        1/ 5TA
+        2/ 3TA
+        T= 5TA+3TA =8TA
+    */
     
     public int Incremento_prestamo() throws SQLException {
         int inc;
         ResultSet rs;
-        rs = bd.consultaBD("SELECT max(idPrestamo) as num FROM prestamo;");
-        if (rs.next()) {
-            inc = rs.getInt(1) + 1;
-        } else {
-            inc = 1;
+        rs = bd.consultaBD("SELECT max(idPrestamo) as num FROM prestamo;");/*3*/
+        if (rs.next()) {                 /*4*/          
+            inc = rs.getInt(1) + 1;      /*5*/
+        } else {                         /*6*/
+            inc = 1;                     /*7*/
         }
         return inc;
     }
+    /* 
+        3/ TA
+        4/ TC
+        5/ TA+TO
+        7/ TA
+       
+        TP = TA+TC+TA+TO = 2TA+TC+TO
+        TM = TA+TC+TA = 2TA+TC
+        TE = 2TA+TC+TO - 2TA+TC = TO
+    */
 
     public void Insertar_prestamo() throws SQLException {
-        String cadena = "insert into prestamo values('" + Incremento_prestamo()+ "','" + getIdUsuario()+ "','" + getIdEjemplar()+ "','" + getFechaprest()+ "','" + getFechadevo()+ "')";
-        bd.ActualizarBD(cadena);
+        String cadena = "insert into prestamo values('" + Incremento_prestamo()+ "','" + getIdUsuario()+ "','" + getIdEjemplar()+
+         "','" + getFechaprest()+ "','" + getFechadevo()+ "')"; /*1*/
+        bd.ActualizarBD(cadena);                               /*2*/
 
     }
+    /* 
+        1/ TA
+        2/ 3TA
+        T= TA+3TA =4TA
+    */
 
     public void Actualizar_prestamo() throws SQLException {
-        String cadena = "update prestamo set idUsuario='" + getIdUsuario()+ "',idEjemplar='" + getIdEjemplar()+ "',fechaprest='" + getFechaprest()+ "', fechadevo ='" + getFechadevo() + "'where idPrestamo ='" +getIdPrestamo()+"'";
-        bd.ActualizarBD(cadena);
-        
+        String cadena = "update prestamo set idUsuario='" + getIdUsuario()+ "',idEjemplar='" + getIdEjemplar()+ 
+        "',fechaprest='" + getFechaprest()+ "', fechadevo ='" + getFechadevo() + "'where idPrestamo ='" +getIdPrestamo()+"'";/*1*/
+        bd.ActualizarBD(cadena);                                                                 /*2*/
                 
     }
+    /* 
+        1/ TA
+        2/ 3TA
+        T= TA+3TA =4TA
+    */
 
     public void Eliminar_prestamo() throws SQLException {
-        String cadena = "delete from prestamo where idPrestamo='" + getIdPrestamo()+"'";
-        bd.ActualizarBD(cadena);
+        String cadena = "delete from prestamo where idPrestamo='" + getIdPrestamo()+"'";/*1*/
+        bd.ActualizarBD(cadena);                                                       /*2*/
     }
+    /* 
+        1/ TA
+        2/ 2TA
+        T= TA+2TA =3TA
+    */
 
     public ResultSet consultaTabla(String sql) throws SQLException {
         return bd.consultaBD(sql);
@@ -127,8 +180,8 @@ public ResultSet obtenerprestamo() throws SQLException {
     
     public void Consultar_prestamo() throws SQLException {
         ResultSet rs;
-        rs = bd.consultaBD("select * from prestamo");
-        while (rs.next()) {
+        rs = bd.consultaBD("select * from prestamo"); /*1*/
+        while (rs.next()) {                           /*2*/
             System.out.print(rs.getInt(1) + " ");
             System.out.print(rs.getInt(2) + " ");
             System.out.print(rs.getInt(3) + " ");
@@ -136,6 +189,13 @@ public ResultSet obtenerprestamo() throws SQLException {
             System.out.print(rs.getString(5) + " ");
             System.out.println("");
         }
+        /* 
+        1/ 2TA
+        2/ N*TC+TC
+        TP= 2TA+N*TC+TC
+        TM= 2TA+TC
+        TE= 2TA+N*TC+TC - 2TA+TC = N*TC
+    */
     }
 
 }
--- BibliotecaCS/BibliotecaCS/bibliotecaCS/src/bibliotecaCS/usuarioBeans.java
+++ BibliotecaCS/BibliotecaCS/bibliotecaCS/src/bibliotecaCS/usuarioBeans.java
@@ -11,20 +11,36 @@ public class usuarioBeans {
     private String direccion;
 
     public void setIdUsuario(int idUsuario) {
-        this.idUsuario = idUsuario;
+        this.idUsuario = idUsuario;/*1*/
     }
+    /* 
+        1/ TA
+        T= TA
+    */
 
     public void setNombre(String nombre) {
-        this.nombre = nombre;
+        this.nombre = nombre;/*1*/
     }
+    /* 
+        1/ TA
+        T= TA
+    */
 
     public void setTelefono(int telefono) {
-        this.telefono = telefono;
+        this.telefono = telefono;/*1*/
     }
+    /* 
+        1/ TA
+        T= TA
+    */
 
     public void setDireccion(String direccion) {
-        this.direccion = direccion;
+        this.direccion = direccion;/*1*/
     }
+    /* 
+        1/ TA
+        T= TA
+    */
 
     public int getIdUsuario() {
         return idUsuario;
@@ -47,46 +63,78 @@ public class usuarioBeans {
     }
 
     public void setBd(accesobd bd) {
-        this.bd = bd;
+        this.bd = bd;/*1*/
     }
+    /* 
+        1/ TA
+        T= TA
+    */
     
     accesobd bd;
 
     public usuarioBeans() throws Exception {
-        bd = new accesobd("localhost", "root", "", "bliblioteca");
-        bd.conectarBD();
+        bd = new accesobd("localhost", "root", "", "bliblioteca");/*1*/
+        bd.conectarBD();                /*1*/
     }
-
-    
+    /* 
+        1/ 5TA
+        2/ 3TA
+        T= 5TA+3TA =8TA
+    */
     public int Incremento_usuario() throws SQLException {
         int inc;
         ResultSet rs;
-        rs = bd.consultaBD("SELECT max(idUsuario) as num FROM usuario;");
-        if (rs.next()) {
-            inc = rs.getInt(1) + 1;
-        } else {
-            inc = 1;
+        rs = bd.consultaBD("SELECT max(idUsuario) as num FROM usuario;");/*3*/
+        if (rs.next()) {             /*4*/
+            inc = rs.getInt(1) + 1; /*5*/
+        } else {                   /*6*/
+            inc = 1;              /*7*/
         }
         return inc;
     }
+    /* 
+        3/ TA
+        4/ TC
+        5/ TA+TO
+        7/ TA
+       
+        TP = TA+TC+TA+TO = 2TA+TC+TO
+        TM = TA+TC+TA = 2TA+TC
+        TE = 2TA+TC+TO - 2TA+TC = TO
+    */
 
     public void Insertar_usuario() throws SQLException {
-        String cadena = "insert into usuario values('" + Incremento_usuario() + "','" + getNombre() + "','" + getTelefono() + "','" + getDireccion() + "')";
-        bd.ActualizarBD(cadena);
+        String cadena = "insert into usuario values('" + Incremento_usuario() + "','" + getNombre() + 
+        "','" + getTelefono() + "','" + getDireccion() + "')"; /*1*/
+        bd.ActualizarBD(cadena);                               /*2*/
 
     }
+    /* 
+        1/ TA
+        2/ 3TA
+        T= TA+3TA =4TA
+    */
 
     public void Actualizar_usuario() throws SQLException {
-        String cadena = "update usuario set nombre='" + getNombre()+ "', telefono='" + getTelefono()+ "', direccion='" + getDireccion()+ "'where idUruario ='" +getIdUsuario()+"'";
-        bd.ActualizarBD(cadena);
-        
-                
+        String cadena = "update usuario set nombre='" + getNombre()+ "', telefono='" + getTelefono()+
+         "', direccion='" + getDireccion()+ "'where idUruario ='" +getIdUsuario()+"'";/*1*/
+        bd.ActualizarBD(cadena);                                                      /*2*/
     }
+    /* 
+        1/ TA
+        2/ 3TA
+        T= TA+3TA =4TA
+    */
 
     public void Eliminar_usuario() throws SQLException {
-        String cadena = "delete from usuario where idUsuario='" + getIdUsuario()+"'";
-        bd.ActualizarBD(cadena);
+        String cadena = "delete from usuario where idUsuario='" + getIdUsuario()+"'";/*1*/
+        bd.ActualizarBD(cadena);                                                    /*2*/
     }
+    /* 
+        1/ TA
+        2/ 2TA
+        T= TA+2TA =3TA
+    */
 
     public ResultSet consultaTabla(String sql) throws SQLException {
         return bd.consultaBD(sql);
@@ -103,14 +151,21 @@ public ResultSet obtenerusuario() throws SQLException {
     
     public void Consultar_usuario() throws SQLException {
         ResultSet rs;
-        rs = bd.consultaBD("select * from usuario");
-        while (rs.next()) {
+        rs = bd.consultaBD("select * from usuario"); /*1*/
+        while (rs.next()) {                         /*2*/
             System.out.print(rs.getInt(1) + " ");
             System.out.print(rs.getString(2) + " ");
             System.out.print(rs.getInt(3) + " ");
             System.out.print(rs.getString(4) + " ");
             System.out.println("");
         }
+        /* 
+        1/ 2TA
+        2/ N*TC+TC
+        TP= 2TA+N*TC+TC
+        TM= 2TA+TC
+        TE= 2TA+N*TC+TC - 2TA+TC = N*TC
+    */
     }
 
 }
