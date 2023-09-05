4e6972c68b238db7b5385229f743f8611b9f34d4
Brigitte2003 <brillitq2003@gmail.com>
2023-09-04 19:03:24 -0500

    Modificacion de Ejemplar y Autor

--- BibliotecaCS/BibliotecaCS/bibliotecaCS/src/bibliotecaCS/autorBeans.java
+++ BibliotecaCS/BibliotecaCS/bibliotecaCS/src/bibliotecaCS/autorBeans.java
@@ -19,15 +19,30 @@ public class autorBeans {
     }
 
     public void setIdAutor(int idAutor) {
-        this.idAutor = idAutor;
+        this.idAutor = idAutor;  /*1*/
+       
+        /* 
+            1/ TA
+            T= TA
+        */
     }
 
     public void setNombres(String nombres) {
-        this.nombres = nombres;
+        this.nombres = nombres;  /*1*/
+       
+        /* 
+            1/ TA
+            T= TA
+        */
     }
 
     public void setBd(accesobd bd) {
-        this.bd = bd;
+        this.bd = bd;  /*1*/
+       
+        /* 
+            1/ TA
+            T= TA
+        */
     }
     
     accesobd bd;
@@ -41,31 +56,54 @@ public class autorBeans {
     public int Incremento_autor() throws SQLException {
         int inc;
         ResultSet rs;
-        rs = bd.consultaBD("SELECT max(idAutor) as num FROM autor;");
-        if (rs.next()) {
-            inc = rs.getInt(1) + 1;
-        } else {
-            inc = 1;
+        rs = bd.consultaBD("SELECT max(idAutor) as num FROM autor;"); /*3*/
+        if (rs.next()) {                                                  /*4*/
+            inc = rs.getInt(1) + 1;                                       /*5*/
+        } else {                                                          /*6*/
+            inc = 1;                                                      /*7*/
         }
         return inc;
+         /* 
+        3/ TA
+        4/ TC
+        5/ TA+TO
+        7/ TA
+       
+        TP = TA+TC+TA+TO = 2TA+TC+TO
+        TM = TA+TC+TA = 2TA+TC
+        TE = 2TA+TC+TO - 2TA+TC = TO
+    */
     }
 
     public void Insertar_autor() throws SQLException {
-        String cadena = "insert into autor values('" + Incremento_autor() + "','" + getNombres() + "')";
-        bd.ActualizarBD(cadena);
-
+        String cadena = "insert into autor values('" + Incremento_autor() + "','" + getNombres() + "')";  /*1*/
+        bd.ActualizarBD(cadena);                                                                          /*2*/
+    /* 
+        1/ TA
+        2/ 3TA
+        T= TA+3TA =4TA
+    */
     }
 
     public void Actualizar_autor() throws SQLException {
-        String cadena = "update autor set nombres='" + getNombres()+ "'where idAutor ='" +getIdAutor()+"'";
-        bd.ActualizarBD(cadena);
-        
+        String cadena = "update autor set nombres='" + getNombres()+ "'where idAutor ='" +getIdAutor()+"'";  /*1*/
+        bd.ActualizarBD(cadena);                                                                             /*2*/
+    /* 
+        1/ TA
+        2/ 3TA
+        T= TA+3TA =4TA
+    */
                 
     }
 
     public void Eliminar_autor() throws SQLException {
-        String cadena = "delete from autor where idAutor='" + getIdAutor()+"'";
-        bd.ActualizarBD(cadena);
+        String cadena = "delete from autor where idAutor='" + getIdAutor()+"'"; /*1*/
+        bd.ActualizarBD(cadena);                                                /*2*/
+        /* 
+        1/ TA
+        2/ 2TA
+        T= TA+2TA =3TA
+    */
     }
 
     public ResultSet consultaTabla(String sql) throws SQLException {
@@ -87,9 +125,16 @@ public class autorBeans {
         rs = bd.consultaBD("select * from Autor");
         while (rs.next()) {
             System.out.print(rs.getInt(1) + " ");
-            System.out.print(rs.getString(2) + " ");
-            System.out.println("");
+            System.out.print(rs.getString(2) + " "); /*1*/
+            System.out.println("");                  /*2*/
         }
+        /* 
+        1/ 2TA
+        2/ N*TC+TC
+        TP= 2TA+N*TC+TC
+        TM= 2TA+TC
+        TE= 2TA+N*TC+TC - 2TA+TC = N*TC
+    */
     }
 
 }
--- BibliotecaCS/BibliotecaCS/bibliotecaCS/src/bibliotecaCS/ejemplarBeans.java
+++ BibliotecaCS/BibliotecaCS/bibliotecaCS/src/bibliotecaCS/ejemplarBeans.java
@@ -9,19 +9,39 @@ public class ejemplarBeans {
     private int localizacion;
 
     public void setIdEjemplar(int idEjemplar) {
-        this.idEjemplar = idEjemplar;
+        this.idEjemplar = idEjemplar;    /*1*/
+       
+        /* 
+            1/ TA
+            T= TA
+        */
     }
 
     public void setIdLibro(int idLibro) {
-        this.idLibro = idLibro;
+        this.idLibro = idLibro;    /*1*/
+       
+        /* 
+            1/ TA
+            T= TA
+        */
     }
 
     public void setLocalizacion(int localizacion) {
-        this.localizacion = localizacion;
+        this.localizacion = localizacion;    /*1*/
+       
+        /* 
+            1/ TA
+            T= TA
+        */
     }
 
     public void setBd(accesobd bd) {
-        this.bd = bd;
+        this.bd = bd;    /*1*/
+       
+        /* 
+            1/ TA
+            T= TA
+        */
     }
 
     public int getIdEjemplar() {
@@ -29,7 +49,12 @@ public class ejemplarBeans {
     }
 
     public int getIdLibro() {
-        return idLibro;
+        return idLibro;    /*1*/
+       
+        /* 
+            1/ TA
+            T= TA
+        */
     }
 
     public int getLocalizacion() {
@@ -52,31 +77,55 @@ public class ejemplarBeans {
     public int Incremento_ejemplar() throws SQLException {
         int inc;
         ResultSet rs;
-        rs = bd.consultaBD("SELECT max(idEjemplar) as num FROM ejemplar;");
-        if (rs.next()) {
-            inc = rs.getInt(1) + 1;
-        } else {
-            inc = 1;
+        rs = bd.consultaBD("SELECT max(idEjemplar) as num FROM ejemplar;"); /*3*/
+        if (rs.next()) {                                                        /*4*/
+            inc = rs.getInt(1) + 1;                                             /*5*/
+        } else {                                                                /*6*/
+            inc = 1;                                                            /*7*/
         }
         return inc;
+    /* 
+        3/ TA
+        4/ TC
+        5/ TA+TO
+        7/ TA
+       
+        TP = TA+TC+TA+TO = 2TA+TC+TO
+        TM = TA+TC+TA = 2TA+TC
+        TE = 2TA+TC+TO - 2TA+TC = TO
+    */
     }
 
     public void Insertar_ejemplar() throws SQLException {
-        String cadena = "insert into ejemplar values('" + Incremento_ejemplar() + "','" + getIdLibro() + "','" +  getLocalizacion()+ "')";
-        bd.ActualizarBD(cadena);
+        String cadena = "insert into ejemplar values('" + Incremento_ejemplar() + "','" + getIdLibro() + "','" +  getLocalizacion()+ "')"; /*1*/
+        bd.ActualizarBD(cadena);                                                                                                          /*2*/
+     /* 
+        1/ TA
+        2/ 3TA
+        T= TA+3TA =4TA
+    */
 
     }
 
     public void Actualizar_ejemplar() throws SQLException {
-        String cadena = "update ejemplar set localizacion='" + getLocalizacion()+ "'where idEjemplar ='" +getIdEjemplar()+"'";
-        bd.ActualizarBD(cadena);
-        
+        String cadena = "update ejemplar set localizacion='" + getLocalizacion()+ "'where idEjemplar ='" +getIdEjemplar()+"'"; /*1*/
+        bd.ActualizarBD(cadena);                                                                                               /*2*/
+    /* 
+        1/ TA
+        2/ 3TA
+        T= TA+3TA =4TA
+    */
                 
     }
 
     public void Eliminar_ejemplar() throws SQLException {
-        String cadena = "delete from ejemplar where idEjemplar='" + getIdEjemplar()+"'";
-        bd.ActualizarBD(cadena);
+        String cadena = "delete from ejemplar where idEjemplar='" + getIdEjemplar()+"'"; /*1*/
+        bd.ActualizarBD(cadena);                                                         /*2*/
+         /* 
+        1/ TA
+        2/ 2TA
+        T= TA+2TA =3TA
+    */
     }
 
     public ResultSet consultaTabla(String sql) throws SQLException {
@@ -100,8 +149,16 @@ public ResultSet obtenerejemplar() throws SQLException {
             System.out.print(rs.getInt(1) + " ");
             System.out.print(rs.getInt(2) + " ");
             System.out.print(rs.getInt(3) + " ");
-            System.out.println("");
+            System.out.println(""); /*1*/ 
+                                   /*2*/
         }
+        /* 
+        1/ 2TA
+        2/ N*TC+TC
+        TP= 2TA+N*TC+TC
+        TM= 2TA+TC
+        TE= 2TA+N*TC+TC - 2TA+TC = N*TC
+    */
     }
 
 }
