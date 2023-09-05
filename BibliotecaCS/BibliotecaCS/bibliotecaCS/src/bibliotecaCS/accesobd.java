763963ddc29a90286b0f704927ec83d8737c7b32
jonathanD28 <dlcsjonathan799@gmail.com>
2023-09-04 21:27:00 -0500

    .

--- BibliotecaCS/BibliotecaCS/bibliotecaCS/src/bibliotecaCS/accesobd.java
+++ BibliotecaCS/BibliotecaCS/bibliotecaCS/src/bibliotecaCS/accesobd.java
@@ -34,35 +34,54 @@ public class accesobd {
     
     public void conectarBD() throws Exception{
         try {
-            Driver driver=(Driver)
-                    Class.forName("com.mysql.jdbc.Driver").newInstance();
-            String cadena="jdbc:mysql://"+getHost()+"/"+getBd();
-            conexion=DriverManager.getConnection(cadena,getUser(),getPasswd());
-           } catch (SQLException e) {
-            System.out.println("Error a la conexion a la Base de Datos"+e.toString());
+            Driver driver=(Driver)                                        /*1*/
+                    Class.forName("com.mysql.jdbc.Driver").newInstance(); /*2*/
+            String cadena="jdbc:mysql://"+getHost()+"/"+getBd();           /*3*/
+            conexion=DriverManager.getConnection(cadena,getUser(),getPasswd()); /*4*/
+           } catch (SQLException e) {             
+            System.out.println("Error a la conexion a la Base de Datos"+e.toString()); 
         }
     }
+    /* 
+        /* 
+        1/ TA
+        2/ 3TA
+        T= TA+3TA =4TA
+    */
+    
+    
     
     public void ActualizarBD(String sql)throws SQLException{
         try {
-            Statement stm=conexion.createStatement();
-            stm.executeUpdate(sql);
-            System.out.println("Transaccion Exitosa");
+            Statement stm=conexion.createStatement(); /*1*/
+            stm.executeUpdate(sql);                   /*2*/
+            System.out.println("Transaccion Exitosa");/*3*/
         } catch (SQLException e) {
-            System.out.println("Error en la Transaccion "+ e.toString());
+            System.out.println("Error en la Transaccion "+ e.toString()); /*4*/
         }
         
     }
+    /* 
+        /* 
+        1/ TA
+        2/ 3TA
+        T= TA+3TA =4TA
+    */
     
     public ResultSet consultaBD(String sql)throws SQLException{
-        ResultSet cursor;
-        Statement stm=conexion.createStatement();
-        cursor=stm.executeQuery(sql);
+        ResultSet cursor;                        /*1*/
+        Statement stm=conexion.createStatement();/*2*/
+        cursor=stm.executeQuery(sql);            /*3*/
         return cursor;
     }
+    /* 
+        1/ TA
+        2/ 2TA
+        T= TA+2TA =3TA
+     */
     
    public void cerrarBD()throws SQLException{
-       conexion.close();
+       conexion.close(); /*1*/
    }
 }
 
